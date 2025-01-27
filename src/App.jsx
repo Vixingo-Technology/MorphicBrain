892ed65538b6f4b9425a51eaaec5912b062b6f8b
Vixingo <nazmul.abbu@gmail.com>
2025-01-28 00:47:35 +0600

    new pages

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { Route, Routes } from "react-router";
 import Landing from "./pages/Landing";
 import About from "./pages/About";
 import Capabilities from "./pages/Capabilities";
+import Signup from "./pages/Signup";
 
 function App() {
     const [count, setCount] = useState(0);
@@ -22,6 +23,24 @@ function App() {
                 <Route path="/whitepaper" element={<Paper>whitepaper</Paper>} />
                 <Route path="/research" element={<Paper>research</Paper>} />
                 <Route path="/capabilities" element={<Capabilities />} />
+                <Route path="/services" element={<Paper>services</Paper>} />
+                <Route path="/team" element={<Paper>team</Paper>} />
+                <Route path="/careers" element={<Paper>careers</Paper>} />
+                <Route path="/blog" element={<Paper>blog</Paper>} />
+                <Route path="/faq" element={<Paper>faq</Paper>} />
+                <Route path="/terms" element={<Paper>terms</Paper>} />
+                <Route path="/privacy" element={<Paper>privacy</Paper>} />
+                <Route path="/login" element={<Paper>login</Paper>} />
+                <Route path="/logout" element={<Paper>logout</Paper>} />
+                <Route path="/signup" element={Signup} />
+                <Route
+                    path="/forgot-password"
+                    element={<Paper>forgot-password</Paper>}
+                />
+                <Route
+                    path="/reset-password"
+                    element={<Paper>reset-password</Paper>}
+                />
                 <Route path="*" element={<Paper>404</Paper>} />
             </Routes>
         </>
