1508fe7044be2f49e6f465740e298c62e9c4ea90
Vixingo <nazmul.abbu@gmail.com>
2025-02-04 22:00:08 +0600

    about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Feature from "../components/Hero/Feature";
 
 function About() {
-    return <div>About</div>;
+    return (
+        <>
+            <Feature />
+        </>
+    );
 }
 
 export default About;
